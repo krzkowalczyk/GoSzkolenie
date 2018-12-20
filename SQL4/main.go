c1a4ef560b17e27ef773699ba1f3a8111dfbe91b
Jan Nowak <jannowak@example.com>
2018-12-20 16:04:33 +0100

    koniec dnia

--- SQL4/main.go
+++ SQL4/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 
 	"fmt"
 	"os"
@@ -55,22 +56,25 @@ func GetPeopleEndpoint(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(people)
 }
 func CreatePersonEndpoint(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
+	// params := mux.Vars(r)
 	var person Person
 	_ = json.NewDecoder(r.Body).Decode(&person)
-	person.ID = params["id"]
-	people = append(people, person)
-	json.NewEncoder(w).Encode(people)
+	// person.ID = params["id"]
+	// people = append(people, person)
+	// json.NewEncoder(w).Encode(people)
+	dodaj(cnstr, person.Name, person.Location)
 }
 func DeletePersonEndpoint(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
-	for index, item := range people {
-		if item.ID == params["id"] {
-			people = append(people[:index], people[index+1:]...)
-			break
-		}
-		json.NewEncoder(w).Encode(people)
-	}
+	// for index, item := range people {
+	// 	if item.ID == params["id"] {
+	// 		people = append(people[:index], people[index+1:]...)
+	// 		break
+	// 	}
+	// 	json.NewEncoder(w).Encode(people)
+	// }
+	tid, _ := strconv.Atoi(params["id"])
+	usun(cnstr, tid)
 }
 
 func main() {
